5d5175f62d9be8943c40b46d948809ce238c41e8
Carlos Richard Geraldine <carlosrichardgeraldine@outlook.com>
2025-10-20 14:24:07 +0700

    Dice Rolls Exercise 2

--- 4_myFourthCode.cs
+++ 4_myFourthCode.cs
@@ -25,10 +25,14 @@ namespace myFourthCode {
                 
                 Console.WriteLine("Final total: " + totalDiceRolls);
 
-                if (totalDiceRolls >= 14) {
-                    Console.WriteLine("You win!\n");
+                if (totalDiceRolls >= 16) {
+                    Console.WriteLine("You win a new car!\n");
+                } else if (totalDiceRolls >= 10) {
+                    Console.WriteLine("You win a new laptop!\n");
+                } else if (totalDiceRolls == 7) {
+                    Console.WriteLine("You win a trip for two!\n");
                 } else {
-                    Console.WriteLine("You lose!\n");
+                    Console.WriteLine("You win a kitten!\n");
                 }
             }
         }
